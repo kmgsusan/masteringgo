6bdf9c1aa8adfe7febf8a2c463425a5cc78cacf1
mgkim <mgkim@cafe24corp.com>
2021-12-20 16:44:33 +0900

    20211220

---
+++ io/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"io"
+	"os"
+)
+
+func main() {
+	myString := ""
+	arguments := os.Args
+
+	if len(arguments) == 1 {
+		myString = " Pleasse "
+	} else {
+		// myString = arguments[1]
+		for i, x := range arguments {
+			if i > 0 {
+				myString += x + ", "
+			}
+		}
+	}
+
+	io.WriteString(os.Stdout, myString)
+}
